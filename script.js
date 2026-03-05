3f0d4fc9ef71383b7137bc8ec990f49cae7f0ab4
abdulrahmaneltahrawi-svg <abdulrahmaneltahrawi@gmail.com>
2026-03-05 13:20:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("test")
+
+
+const openBtn =document.getElementById('openModel');
+const model =document.getElementById('loginModel');
+const closeBtn =document.getElementById('closeModel');
+
+if (openBtn){
+    openBtn.onclick =function(){
+        model.style.display ='flex';
+    };
+}
+
+if (closeModel){
+    closeModel.onclick =function(){
+        model.style.display ='none';
+    };
+}
+
+window.onclick =function(event){
+    if (event.target ===model) {
+        model.style.display ='none';
+    }
+
+};
